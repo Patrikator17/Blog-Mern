54afc574bb69771c4088905e29dfea8fdfb67db4
Patrikator17 <patkarpratik42>
2024-02-16 14:12:23 +0530

    Create Routes, Controller

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import mongoose from 'mongoose';
 import dotenv from 'dotenv'
+import userRoutes from './routes/user.routes.js';
 
 dotenv.config()
 
@@ -13,6 +14,8 @@ mongoose.connect(process.env.MONGODB)
 app.listen(3000, () => {
     console.log('Server Running on port 3000..');
 })
+
+app.use('/api/user', userRoutes)
 
 
 //pratikMernBlog
