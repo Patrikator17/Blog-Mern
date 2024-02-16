d0e74db1e9d30b7cd1810301213bde39d4fe5e00
Patrikator17 <patkarpratik42>
2024-02-16 13:17:42 +0530

    Setup MongoDB and dotenv

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,18 @@
 import express from 'express'
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const app = express()
+
+mongoose.connect(process.env.MONGODB)
+.then(() => console.log('Mongo DB Connected...'))
+.catch((err) => console.log(err))
 
 app.listen(3000, () => {
     console.log('Server Running on port 3000..');
 })
+
+
+//pratikMernBlog
