398bd4c3e561817c330d57e3de19421e032be2dd
Patrikator17 <patkarpratik42>
2024-02-17 13:13:39 +0530

    ErrorHandler middleware to handle errors

--- backend/controller/auth.controller.js
+++ backend/controller/auth.controller.js
@@ -1,40 +1,40 @@
-import User from "../model/user.model.js"
-import bcryptjs from 'bcryptjs'
+import User from "../model/user.model.js";
+import bcryptjs from 'bcryptjs';
+import { errorHandler } from "../utils/errorHandle.js";
 
-export const signup = async(req, res) => {
-    // console.log(req.body);
+export const signup = async(req, res, next) => {
+
+    console.log(req.body);
 
     const {
         username,
         email,
         password
-    } = req.body
+    } = req.body;
 
-    if(!username ||
+    if (!username ||
         !email ||
         !password ||
         username === '' ||
         email === '' ||
-        password === ''){
-            return res.status(400).json({message: 'All fields are required'})
-        }
+        password === '') {
+            return next(errorHandler(400, 'All fields are required')); // To prevent the user from being added to the database 
+            //when there's an error, you should return from the function immediately after sending the error response.
+    }
 
-        //After getting password use bcryptjs
+    // After getting password use bcryptjs
+    const hashedPassword = bcryptjs.hashSync(password, 10);
 
-        const hashedPassword = bcryptjs.hashSync(password, 10);
+    const newUser = new User({
+        username,
+        email,
+        password: hashedPassword,
+    });
 
-        const newUser = new User({
-            username,
-            email,
-            password: hashedPassword,
-        })
-
-        try{
-            await newUser.save()
-            res.json('Signup Successful...')
-
-        }catch(error){
-            res.status(500).json({message: error.message})
-        }
-
-}
+    try {
+        await newUser.save();
+        res.json('Signup Successful...');
+    } catch(error) {
+        next(error);
+    }
+};
