9bcfbb1c182b78abf2ba7b2d32d52053a41884b5
Patrikator17 <patkarpratik42>
2024-02-18 12:03:52 +0530

    Create Footer UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,7 @@ import Signup from './pages/Signup'
 import Login from './pages/Login'
 import Project from './pages/Project'
 import Header from './components/Header'
+import Footer from './components/Footer'
 
 
 function App() {
@@ -21,6 +22,7 @@ function App() {
         <Route path='/login' element={<Login />} />
         <Route path='/project' element={<Project />} />
       </Routes>
+    <Footer />
     </BrowserRouter>
   )
 }
---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,107 @@
+import React from 'react'
+import {Footer} from 'flowbite-react'
+import { Link } from 'react-router-dom'
+import { BsFacebook, BsGithub, BsInstagram, BsLinkedin, BsTwitter} from 'react-icons/bs'
+
+const FooterComponent = () => {
+  return (
+    <Footer container className='border border-t-8  border-blue-900'>
+        <div className='w-full max-w-7xl mx-auto'>
+            <div className='grid w-full justify-between sm:flex md: grid-cols-1'>
+                <div className='mt-5'>
+                {/* Logo */}
+                  <Link to='/' className='self-center whitespace-nowrap text-sm 
+                    sm:text-xl font-semibold dark:text-white'>
+                    <span className='px-2 py-1  bg-gradient-to-r from-blue-400 to-purple-600
+                    hover:from-pink-500 hover:to-yellow-500 text-white rounded-xl'>Pratik's</span>
+                      &nbsp; Blog
+                  </Link>
+                </div>
+
+                <div className='grid grid-cols-2 gap-8 sm: mt-4 sm:grid-cols-3 sm:gap-6'>
+                  <div>
+                    <Footer.Title title='About' />
+                    <Footer.LinkGroup col>
+                      <Footer.Link 
+                      href='https://www.google.com'
+                      target='_blank'
+                      rel='noopener noreferer'
+                      >
+                        Domain Summary
+                      </Footer.Link>
+
+                      <Footer.Link 
+                      href='https://www.google.com'
+                      target='_blank'
+                      rel='noopener noreferer'
+                      >
+                        Resume
+                      </Footer.Link>
+                    </Footer.LinkGroup>
+ 
+                  </div>
+                  <div>
+                    <Footer.Title title='Follow Us' />
+                    <Footer.LinkGroup col>
+                      <Footer.Link 
+                      href='https://www.google.com'
+                      target='_blank'
+                      rel='noopener noreferer'
+                      >
+                        Github
+                      </Footer.Link>
+
+                      <Footer.Link 
+                      href='https://www.google.com'
+                      target='_blank'
+                      rel='noopener noreferer'
+                      >
+                        Linkedin
+                      </Footer.Link>
+                    </Footer.LinkGroup>
+ 
+                  </div>
+                  <div>
+                    <Footer.Title title='Legal' />
+                    <Footer.LinkGroup col>
+                      <Footer.Link 
+                      href='https://www.google.com'
+                      target='_blank'
+                      rel='noopener noreferer'
+                      >
+                        Privacy Policy
+                      </Footer.Link>
+
+                      <Footer.Link 
+                      href='https://www.google.com'
+                      target='_blank'
+                      rel='noopener noreferer'
+                      >
+                        Terms and Conditions
+                      </Footer.Link>
+                    </Footer.LinkGroup>
+ 
+                  </div>
+
+
+                </div>
+            </div>
+            <Footer.Divider />
+            <div className='w-full sm:flex sm:items-center sm:justify-between'> 
+              <Footer.Copyright href='#' by='Pratik Patkar' year={new Date().getUTCFullYear()}/>
+            
+
+              <div className='flex gap-6 sm:mt-0 mt-4 sm:justify-center'>
+                <Footer.Icon href='#' icon={BsFacebook}/>
+                <Footer.Icon href='#' icon={BsInstagram}/>
+                <Footer.Icon href='#' icon={BsTwitter}/>
+                <Footer.Icon href='#' icon={BsGithub}/>
+                <Footer.Icon href='#' icon={BsLinkedin}/>
+              </div>
+            </div>
+        </div>
+    </Footer>
+  )
+}
+
+export default FooterComponent
