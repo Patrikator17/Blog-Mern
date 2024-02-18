dccb93edd0500997c78e8d7e9adc23bcdf200145
Patrikator17 <patkarpratik42>
2024-02-18 15:59:37 +0530

    Create Login Backend

--- backend/controller/auth.controller.js
+++ backend/controller/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../model/user.model.js";
 import bcryptjs from 'bcryptjs';
 import { errorHandler } from "../utils/errorHandle.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async(req, res, next) => {
 
@@ -38,3 +39,42 @@ export const signup = async(req, res, next) => {
         next(error);
     }
 };
+
+
+export const login = async(req, res, next) => {
+    const {
+        email,
+        password
+    } = req.body;
+
+    if (!email ||
+        !password ||
+        email === '' ||
+        password === ''){
+            return next(errorHandler(400, 'All fields are required'));
+    }
+
+    try{
+        const validUser = await User.findOne({email})
+        if(!validUser){
+            return next(errorHandler(404, 'User not found..'))
+        }
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        if(!validPassword){
+            return next(errorHandler(400, 'Incorrect Credentials...'))
+        }
+        // Authenticate user
+        const token = jwt.sign(
+            {id: validUser._id}, process.env.JWT_SECRET_KEY, 
+        )
+        //Separate password for display
+        const {password: pass, ...rest} = validUser._doc;
+        
+
+        res.status(200).cookie('access_token', token, {
+            httpOnly: true
+        }).json(rest)
+    }catch(error){
+        next(error)
+    }
+}
--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -5,7 +5,7 @@ import { BsFacebook, BsGithub, BsInstagram, BsLinkedin, BsTwitter} from 'react-i
 
 const FooterComponent = () => {
   return (
-    <Footer container className='border border-t-8  border-blue-900'>
+    <Footer container className='border border-t-4 border-r-indigo-300  border-blue-900'>
         <div className='w-full max-w-7xl mx-auto'>
             <div className='grid w-full justify-between sm:flex md: grid-cols-1'>
                 <div className='mt-5'>
