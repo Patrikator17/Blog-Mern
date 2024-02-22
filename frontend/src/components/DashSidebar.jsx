b20de4403ad94f295153ffd413986cea12deb97a
Patrikator17 <patkarpratik42>
2024-02-22 11:23:03 +0530

    DashBoard- DashSidebar UI

---
+++ frontend/src/components/DashSidebar.jsx
@@ -0,0 +1,46 @@
+import { Sidebar } from 'flowbite-react'
+import React, { useEffect, useState } from 'react'
+import {HiArrowSmRight, HiUser} from 'react-icons/hi'
+import { Link, useLocation } from 'react-router-dom'
+
+const DashSidebar = () => {
+    
+    const location = useLocation()
+
+    const [tab, setTab] = useState('')
+    useEffect(() => {
+        const urlParams = new URLSearchParams(location.search)
+        const tabFromUrl = urlParams.get('tab')
+        // console.log(tabFromUrl);
+    
+    if(tabFromUrl){ 
+      setTab(tabFromUrl)
+    } // to show dahboard only if profile is needed to display
+  }, [location.search])//why search
+
+
+
+  return (
+    <Sidebar className='w-full md:w-56'>
+        <Sidebar.Items>
+            <Sidebar.ItemGroup>
+                <Link to='/dashboard?tab=profile'>
+                    <Sidebar.Item 
+                    active={tab==='profile'} 
+                    icon={HiUser} 
+                    label={'User'} 
+                    labelColor='dark'>
+                        Profile
+                    </Sidebar.Item>
+                </Link>
+
+                <Sidebar.Item  icon={HiArrowSmRight} className='cursor-pointer'>
+                    Signout
+                </Sidebar.Item>
+            </Sidebar.ItemGroup>
+        </Sidebar.Items>
+    </Sidebar>
+  )
+}
+
+export default DashSidebar
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,8 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import {useLocation} from 'react-router-dom'
+import DashSidebar from '../components/DashSidebar'
+import DashProfile from '../components/DashProfile'
 
 const Dashboard = () => {
+
+  const location = useLocation()
+
+  const [tab, setTab] = useState('')
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search)
+    const tabFromUrl = urlParams.get('tab')
+    // console.log(tabFromUrl);
+    
+    if(tabFromUrl){ 
+      setTab(tabFromUrl)
+    } // to show dahboard only if profile is needed to display
+
+  }, [location.search])//why search
+
+
   return (
-    <div>Dashboard</div>
+    <>
+    <div className='min-h-screen flex flex-col md:flex-row'>
+      {/* Sidebar */}
+        <div className='md:w-56'>
+          <DashSidebar />
+        </div>
+
+        {/* Profile */}
+        {tab === 'profile' && <DashProfile />}
+
+    </div>
+    </>
   )
 }
 
