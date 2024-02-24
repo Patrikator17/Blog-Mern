6a359754ddddf3a3a13bbb204c410283cbabc1ce
Patrikator17 <patkarpratik42>
2024-02-24 15:54:47 +0530

    Delete User Backend

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,11 +1,12 @@
 import express from 'express'
 const router = express.Router()
-import { test, updateUser } from '../controller/user.controller.js'
+import { deleteUser, test, updateUser } from '../controller/user.controller.js'
 import { verifyToken } from '../utils/verifyUser.js'
 
 
 router.get('/test', test)
 router.put('/update/:userId', verifyToken, updateUser)
+router.delete('/delete/:userId', verifyToken, deleteUser)
 
 
 export default router
