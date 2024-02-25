629694ec76a7b48ee947a251109ea6af023a161a
Patrikator17 <patkarpratik42>
2024-02-25 13:50:07 +0530

    add admin backend to user

--- backend/controller/auth.controller.js
+++ backend/controller/auth.controller.js
@@ -65,7 +65,7 @@ export const login = async(req, res, next) => {
         }
         // Authenticate user
         const token = jwt.sign(
-            {id: validUser._id}, process.env.JWT_SECRET_KEY, 
+            {id: validUser._id, isAdmin: validUser.isAdmin}, process.env.JWT_SECRET_KEY, //add isAdmin in cookies
         )
         //Separate password for display
         const {password: pass, ...rest} = validUser._doc;
@@ -85,7 +85,7 @@ export const google = async(req, res, next) => {
     try{
         const user = await User.findOne({email})
         if(user){
-            const token = jwt.sign({id: user._id}, process.env.JWT_SECRET_KEY);
+            const token = jwt.sign({id: user._id, isAdmin: user.isAdmin}, process.env.JWT_SECRET_KEY);
             const {password, ...rest} = user._doc; // separate password from the rest
             res.status(200).cookie('access_token', token, {
                 httpOnly: true,
@@ -101,7 +101,7 @@ export const google = async(req, res, next) => {
                 profilePicture: googlePhotoUrl
             })
             await newUser.save()
-            const token = jwt.sign({id: newUser._id}, process.env.JWT_SECRET_KEY);
+            const token = jwt.sign({id: newUser._id, isAdmin: newUser.isAdmin}, process.env.JWT_SECRET_KEY);
             const {password, ...rest} = newUser._doc; // separate password from the rest
             res.status(200).cookie('access_token', token, {
                 httpOnly: true,
--- backend/model/user.model.js
+++ backend/model/user.model.js
@@ -19,6 +19,10 @@ const userSchema = new mongoose.Schema({
         type:  String,
         default: 'https://w7.pngwing.com/pngs/304/275/png-transparent-user-profile-computer-icons-profile-miscellaneous-logo-monochrome-thumbnail.png'
     },
+    isAdmin:{
+        type: Boolean,
+        default: false
+    },
 
 }, {timestamps: true}
 )
