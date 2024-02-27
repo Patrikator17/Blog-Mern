f19c0548f353d78478eefec4bf522aac0ba64641
Patrikator17 <patkarpratik42>
2024-02-27 18:17:10 +0530

    Create getPost api route

--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -1,9 +1,10 @@
 import express from 'express'
 import { verifyToken } from '../utils/verifyUser.js'
-import { create } from '../controller/post.controller.js'
+import { create, getPost } from '../controller/post.controller.js'
 
 const router = express.Router()
 
 router.post('/create-post', verifyToken, create)
+router.get('/get-post', getPost)
 
 export default router
